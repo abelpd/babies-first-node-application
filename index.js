a6e1adc57bc189d03207773011318b3c49134bc1
DA <abelpd07@gmail.com>
2020-05-23 12:49:41 -0400

    adding express server

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const http = require('http')
-const hostname = 'localhost'
+const express = require('express');
+const http = require('http');
+
+const fs = require('fs');
+const path = require('path');
+
+const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.header);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
-})
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+    
 
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}: ${port}`)
-}); 
+    console.log(`Server running at http://${hostname}:${port}`)
+});
